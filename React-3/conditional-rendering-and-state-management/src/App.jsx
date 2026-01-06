43d5f5ac42e0f6f7d77986510b2c21f72966c3ad
Md Imran <samimdimran786@gmail.com>
2026-01-06 14:19:18 +0530

    Event Handling

--- React-3/conditional-rendering-and-state-management/src/App.jsx
+++ React-3/conditional-rendering-and-state-management/src/App.jsx
@@ -1,9 +1,11 @@
+import Button from "./components/Button";
 import Message from "./components/Message";
 
 function App() {
   return (
     <>
-      <Message isLoggedIn={true} />
+      {/* <Message isLoggedIn={true} /> */}
+      <Button></Button>
     </>
   );
 }
---
+++ React-3/conditional-rendering-and-state-management/src/components/Button.jsx
@@ -0,0 +1,12 @@
+const Button = () => {
+  const handleClick = () => {
+    console.log("Button clicked!");
+  };
+  return (
+    <>
+      <button onClick={handleClick}>Click Me</button>
+    </>
+  );
+};
+
+export default Button;
