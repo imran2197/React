caa7ef20fc4f5101f941964361b97a98b965c962
Md Imran <samimdimran786@gmail.com>
2026-01-29 12:27:01 +0530

    useMemo hook

--- React-9/memoization/src/Counter/Counter.jsx
+++ React-9/memoization/src/Counter/Counter.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import { useState } from "react";
 import Happy from "./Happy";
 import Person from "./Person";
+import { useMemo } from "react";
+
+const largeNumsArray = () => {
+  console.log("Executing LargeNumsArray Function");
+  const res = [];
+  for (let i = 1; i <= 100000; i++) {
+    res.push(i);
+  }
+  return res;
+};
+
+const sum = (arr) => {
+  console.log("Executing Sum Function");
+  return arr.reduce((acc, currVal) => acc + currVal, 0);
+};
 
 const Counter = () => {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("Max");
   console.log("Counter Re-rendered");
+
+  const largeArr = useMemo(() => largeNumsArray(), []);
+  const totalSum = useMemo(() => sum(largeArr), [largeArr]);
   return (
     <div>
       <h1>Count: {count}</h1>
@@ -15,6 +33,7 @@ const Counter = () => {
       <Happy />
       <Person name={name} />
       <button onClick={() => setName("Md Imran")}>Change Name</button>
+      <h5>Sum: {totalSum}</h5>
     </div>
   );
 };
